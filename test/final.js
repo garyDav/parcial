8f45e26ffee8f60be5e81cb5ee9073a65210c64c
garyDav <gary.2810.dav@gmail.com>
2020-09-18 10:26:50 -0400

    passing

--- test/final.js
+++ test/final.js
@@ -1,5 +1,9 @@
 const assert = require("assert")
 
+const {
+  fizzbuzz
+} = require("../src/final")
+
 describe.only("Kata Fizz Buzz", () => {
   it("Retornar un numero", () => {
     assert.strictEqual(fizzbuzz(1), 1)
